df7b9d1d0422a73e927ee41ecda6d6814357c37c
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 15:15:58 +0300

    Update shout_cloud.py

--- src/shout_cloud.py
+++ src/shout_cloud.py
@@ -1,17 +1,15 @@
-from requests import post
+from requests import Session
 
 class ShoutCloud:
 	def __init__(self) -> None:
 		self.api = "HTTP://API.SHOUTCLOUD.IO/V1"
-		self.headers = {
-			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+		self.session = Session()
+		self.session.headers = {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 		}
 
 	def capitalize_text(self, text: str) -> dict:
 		data = {
 			"INPUT": text
 		}
-		return post(
-			f"{self.api}/SHOUT",
-			json=data,
-			headers=self.headers).json()
+		return self.session.post(f"{self.api}/SHOUT", json=data).json()
